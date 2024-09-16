b905233af3e502b58d62a90122eddbd8eb03e7f7
SMAWAHID <99089235+SMAWAHID@users.noreply.github.com>
2024-09-16 23:04:03 +0500

    Update and rename Problem1.cpp to Problem1.c

--- Problem1.c
+++ Problem1.c
@@ -1,5 +1,5 @@
 //Write a C program to check whether a number is multiple of 3 or not. 
-//If it is then print “This number is multiple of 3”, otherwise print “This number is not multiple of 3”.
+//If it is then print â€œThis number is multiple of 3â€, otherwise print â€œThis number is not multiple of 3â€.
 
 #include <stdio.h>
 
