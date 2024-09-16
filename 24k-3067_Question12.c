ae781721eb268af7175a7ed1299b76e3fc07f54d
SMAWAHID <99089235+SMAWAHID@users.noreply.github.com>
2024-09-17 00:26:24 +0500

    Update and rename Problem12.cpp to 24k-3067_Question12.c

--- 24k-3067_Question12.c
+++ 24k-3067_Question12.c
@@ -8,20 +8,20 @@ int main(){
 	scanf("%d",&Age);
 		
 	if(Age >= 0 && Age <= 12){
-			printf("Child");
-		}
+		printf("Child");
+	}
 	else if(Age > 12 && Age <= 19){
-			printf("Teenager");
-		}
+		printf("Teenager");
+	}
 	else if(Age > 19 && Age <= 64){
-			printf("Adult");
-		}
+		printf("Adult");
+	}
 	else if(Age > 64){
-			printf("Senior");
-		}		
+		printf("Senior");
+	}		
 	else{
-			printf("Invalid Age");
-		}
+		printf("Invalid Age");
+	}
 		
 	return 0;
 }
