ad85d728bdc0175c6422d674e259ab460a6e6dbd
SMAWAHID <99089235+SMAWAHID@users.noreply.github.com>
2024-09-16 23:22:50 +0500

    Update 24K-3067_Question5.c

--- 24K-3067_Question5.c
+++ 24K-3067_Question5.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 
-int main(){
-int id, units, amountCharged, surchargeAmount;
-float  charge, netAmount;
+int main() {
+    int id, units;
+    float amountCharged, surchargeAmount = 0, charge, netAmount;
 
-printf("Enter your Id: ");
-scanf("%d", &id);
+    printf("Enter your Id: ");
+    scanf("%d", &id);
 
-printf("Enter Units used: ");
-scanf("%d", &units);
+    printf("Enter Units used: ");
+    scanf("%d", &units);
 
-if (units<199){ 
-	charge= 16.20;
-	amountCharged = units * 16.20;
-	netAmount = amountCharged;
-}
-else if(units>=200 && units<300){
-	amountCharged = units * 20.10;
-	netAmount = amountCharged;
-	charge = 20.10;
-}
-else if(units>=300 && units<500){
-	amountCharged = units * 27.10;
-	netAmount = amountCharged;
-	charge = 27.10;
-}
-else if(units>=500){
-	amountCharged = units * 35.90;
-	netAmount = amountCharged;
-	charge = 35.90;
-}
-if (amountCharged>18000){
-	surchargeAmount = (amountCharged*15/100);
-	netAmount = amountCharged + surchargeAmount;
-}
+    if (units < 199) { 
+        charge = 16.20;
+        amountCharged = units * charge;
+    } 
+    else if (units >= 200 && units < 300) {
+        charge = 20.10;
+        amountCharged = units * charge;
+    } 
+    else if (units >= 300 && units < 500) {
+        charge = 27.10;
+        amountCharged = units * charge;
+    } 
+    else if (units >= 500) {
+        charge = 35.90;
+        amountCharged = units * charge;
+    }
 
-printf("Customer ID: %d\n", id);
-printf("Units Consumed: %d\n", units);
-printf("Amount charges @Rs. %.2f per unit: %d\n", charge, amountCharged);
-printf("Surcharge Amount: %d\n", surchargeAmount);
-printf(" Net Amount paid by the Customer: %.2f\n", netAmount);
+    netAmount = amountCharged;
 
-return 0;
+    if (amountCharged > 18000) {
+        surchargeAmount = amountCharged * 0.15;
+        netAmount = amountCharged + surchargeAmount;
+    }
+
+    printf("Customer ID: %d\n", id);
+    printf("Units Consumed: %d\n", units);
+    printf("Amount charges @Rs. %.2f per unit: %.2f\n", charge, amountCharged);
+    printf("Surcharge Amount: %.2f\n", surchargeAmount);
+    printf("Net Amount paid by the Customer: %.2f\n", netAmount);
+
+    return 0;
 }
